bdba8531dc352ffba1d5af6df0558e296bae2d24
harrelfe <harrelfe@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-31 20:26:32 +0000

    improved tests
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/rms@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/survfit.cph.s
+++ pkg/tests/survfit.cph.s
@@ -1,43 +1,56 @@
-plots      <- FALSE
+require(survival)
+
+plots      <- TRUE
+topdf      <- TRUE
+testrms    <- FALSE
 testDesign <- FALSE
+additive   <- FALSE
+roundt     <- FALSE
+chkpts     <- FALSE
 
 ## Simulate a small example to compare results with survival package
-n <- 100
+nfemale <- 100
+nmale   <- 9*nfemale
+n <- nfemale + nmale
 set.seed(1)
 age <- 50 + 12*rnorm(n)
-sex <- factor(sample(c('Male','Female'), n, rep=TRUE))
+sex <- as.factor(c(rep('Male', nmale), rep('Female', nfemale)))
 cens <- 15*runif(n)
-h <- .02*exp(.04*(age-50)+.8*(sex=='Female'))
+h <- .02*exp(.04*(age-50)+1.5*(sex=='Female'))
 dt <- -log(runif(n))/h
 e <- ifelse(dt <= cens, 1, 0)
-dt <- round(pmin(dt, cens))
+dt <- if(roundt) round(pmin(dt, cens)) else pmin(dt, cens)
 dtmax <- tapply(dt, sex, max)
-require(survival)
 Srv <- Surv(dt, e)
-f <- coxph(Srv ~ age*strata(sex))
+f <- coxph(if(additive) Srv ~ age + strata(sex) else Srv ~ age*strata(sex))
 levels(sex)
 new <- expand.grid(age=50, sex=levels(sex))
-
 new1 <- new[1,]
 new2 <- new[2,]
 
 if(plots)
   {
+    if(topdf) pdf('/tmp/z.pdf')
     gr <- function(col=gray(.9), lwd=1)
       abline(h=seq(.2,.8,by=.2), col=col, lwd=lwd)
     par(mfrow=c(2,2))
 
-    s <- survfit(f, new1)
-    plot(s, conf.int=TRUE, main=new1$sex)
+    s <- survfit(f, new1, censor=FALSE)
+    plot(s, conf.int=TRUE,
+         main=paste(new1$sex, 'coxph survfit newdata new1'))
     lines(s, col='red')
     gr()
-    s <- survfit(f, new2)
-    plot(s, conf.int=TRUE, main=new2$sex)
+    s <- survfit(f, new2, censor=FALSE)
+    plot(s, conf.int=TRUE,
+         main=paste(new2$sex, 'coxph survfit newdata new2'))
     lines(s, col='red')
     gr()
   }
 
-s  <- survfit(f, new)
+s  <- survfit(f, new, censor=FALSE)
+plot(s, main='coxph combined newdata plot.survfit')
+gr()
+
 st <- rep(names(s$strata), s$strata)
 i <- 0
 for(sx in levels(sex))
@@ -60,8 +73,9 @@ if(plots)
     with(subset(z, strata=='sex=Female'),
          {
            plot(c(time,dtmax['Female']), c(surv.1, min(surv.1)),
-                type='s', col='red', xlim=c(0,15), ylim=c(0,1))
-           points(time, surv.1, pch='f', col='red')
+                type='s', col='red', xlim=c(0,15), ylim=c(0,1),
+                xlab='', ylab='')
+           if(chkpts) points(time, surv.1, pch='f', col='red')
            lines(time, lower.1, type='s', col='red')
            lines(time, upper.1, type='s', col='red')
          })
@@ -69,69 +83,79 @@ if(plots)
          {
            lines(c(time,dtmax['Male']), c(surv.2, min(surv.2)),
                  type='s', col='green')
-           points(time, surv.2, pch='m', col='green')
+           if(chkpts) points(time, surv.2, pch='m', col='green')
            lines(time, lower.2, type='s', col='green')
            lines(time, upper.2, type='s', col='green')
          })
+    title('coxph combined newdata manual')
     gr()
   }
 
-require(rms)
-dd <- datadist(age,sex); options(datadist='dd')
-Srv <- Surv(dt, e)
-g <- cph(Srv ~ age*strat(sex), surv=TRUE)
-
-for(sx in levels(sex))
+if(testrms)
   {
-    k <- survest(g, data.frame(age=50, sex=sx))
-    cat(sx, '\t', 'survest surv=TRUE\n')
-    print(with(k, data.frame(time=time, surv=surv,
-                             std.err=std.err, lower=lower, upper=upper)))
-  }
-
-if(plots)
-  {
-    survplot(g, sex=., age=50, conf.int=TRUE)
-    w <-survest(g, data.frame(age=50, sex='Female'))
-    points(w$time, w$surv, pch='f')
-    w <- survest(g, data.frame(age=50, sex='Male'))
-    points(w$time, w$surv, pch='m')
-    gr(col='blue', lwd=.2)
-  }
-
-h <- cph(Srv ~ age*strat(sex), x=TRUE, y=TRUE)
-s <- survfit(h, new)
-unclass(s)
-st <- rep(names(s$strata), s$strata)
-i <- 0
-for(sx in levels(sex))
-  {
-    i <- i + 1
-    cat(sx, '\t', 'survfit.cph surv=F\n')
-    j <- st==paste('sex=', sx, sep='')
-    z <- with(s, data.frame(time=time[j], surv=surv[j,i],
-                            std.err=std.err[j,i],
-                            lower=lower[j,i],
-                            upper=upper[j,i]))
-    print(z)
-    k <- survest(h, data.frame(age=50, sex=sx))
-    cat(sx, '\t', 'survest surv=F\n')
-    print(with(k, data.frame(time=time, surv=surv,
-                             std.err=std.err, lower=lower, upper=upper)))
-  }
+    require(rms)
+    system('cat ~/R/rms/pkg/R/*.s > /tmp/rms.s')
+    source('/tmp/rms.s')
+    dd <- datadist(age,sex); options(datadist='dd')
+    Srv <- Surv(dt, e)
+    g <- cph(if(additive) Srv ~ age + strat(sex) else Srv ~ age*strat(sex),
+             surv=TRUE)
+    
+    for(sx in levels(sex))
+      {
+        k <- survest(g, data.frame(age=50, sex=sx))
+        cat(sx, '\t', 'survest surv=TRUE\n')
+        print(with(k, data.frame(time=time, surv=surv,
+                                 std.err=std.err, lower=lower, upper=upper)))
+      }
+    
+    if(plots)
+      {
+        survplot(g, sex, age=50, conf.int=TRUE)
+        w <-survest(g, data.frame(age=50, sex='Female'))
+        if(chkpts) points(w$time, w$surv, pch='f')
+        w <- survest(g, data.frame(age=50, sex='Male'))
+        if(chkpts) points(w$time, w$surv, pch='m')
+        title('rms survplot + survest surv=T')
+        gr()
+      }
+    
+    h <- cph(if(additive) Srv ~ age + strat(sex) else Srv ~ age*strat(sex),
+             x=TRUE, y=TRUE)
+    s <- survfit(h, new)
+    unclass(s)
+    st <- rep(names(s$strata), s$strata)
+    i <- 0
+    for(sx in levels(sex))
+      {
+        i <- i + 1
+        cat(sx, '\t', 'survfit.cph surv=F\n')
+        j <- st==paste('sex=', sx, sep='')
+        z <- with(s, data.frame(time=time[j], surv=surv[j,i],
+                                std.err=std.err[j,i],
+                                lower=lower[j,i],
+                                upper=upper[j,i]))
+        print(z)
+        k <- survest(h, data.frame(age=50, sex=sx))
+        cat(sx, '\t', 'survest surv=F\n')
+        print(with(k, data.frame(time=time, surv=surv,
+                                 std.err=std.err, lower=lower, upper=upper)))
+      }
 
 ## i <- s2$strata
 ## with(s2, data.frame(time=time[,i], surv=surv[,i], se=std.err[,i],
 ##                          lower=lower[,i], upper=upper[,i]))
 
-if(plots)
-  {
-    survplot(h, sex=., age=50, conf.int=TRUE)
-    w <- survest(h, data.frame(age=50, sex='Female'))
-    points(w$time, w$surv, pch='f')
-    w <- survest(h, data.frame(age=50, sex='Male'))
-    points(w$time, w$surv, pch='m')
-    gr(col='blue', lwd=.2)
+    if(plots)
+      {
+        survplot(h, sex, age=50, conf.int=TRUE)
+        w <- survest(h, data.frame(age=50, sex='Female'))
+        if(chkpts) points(w$time, w$surv, pch='f')
+        w <- survest(h, data.frame(age=50, sex='Male'))
+        if(chkpts) points(w$time, w$surv, pch='m')
+        title('rms survplot + survest x=T y=T')
+        gr()
+      }
   }
 
 if(testDesign)
@@ -141,28 +165,15 @@ if(testDesign)
     Srv <- Surv(dt, e)
     new <- expand.grid(age=50, sex=levels(sex))
     dd <- datadist(age,sex); options(datadist='dd')
-    ## Temporary fix until Design updated around 10Sep09
-    Varcov <- function(object, ...) UseMethod("Varcov")
-    Varcov.cph <- function(object, regcoef.only=FALSE, ...)
-      Varcov.default(object, regcoef.only, ...)
-    Varcov.default <- function(fit, regcoef.only=FALSE)
-      {
-        vc <- fit$Varcov
-        if(length(vc)) {
-          if(regcoef.only) return(fit$var) else
-          return(vc(fit,which='var'))
-        }
-        cov <- fit$var
-        if(is.null(cov)) stop("fit does not have variance-covariance matrix")
-        if(regcoef.only)
-          {
-            p <- length(fit$coefficients)  # 14Sep00
-            cov <- cov[1:p, 1:p, drop=FALSE]
-          }
-        cov
-      }
   
-    g <- cph(Srv ~ age*strat(sex), surv=TRUE)
+    g <- cph(if(additive) Srv ~ age + strat(sex) else Srv ~ age*strat(sex),
+             surv=TRUE)
+    if(plots)
+      {
+        survplot(g, sex=NA, age=50, conf.int=TRUE, conf='bands')
+        gr()
+        title('Design survplot surv=T')
+      }
 
     options(digits=3)
     for(sx in levels(sex))
@@ -172,8 +183,15 @@ if(testDesign)
         print(with(k, data.frame(time=time, surv=surv,
                                  std.err=std.err, lower=lower, upper=upper)))
       }
-    g <- cph(Srv ~ age*strat(sex), x=TRUE, y=TRUE)
+    g <- cph(if(additive) Srv ~ age + strat(sex) else Srv ~ age*strat(sex),
+             x=TRUE, y=TRUE)
     cat('cph x=T y=T survfit\n')
     print(unclass(survfit(g, new, conf.type='log')))
-    
+    if(plots)
+      {
+        survplot(g, sex=NA, age=50, conf.int=TRUE, conf='bands')
+        title('Design survplot x=T y=T')
+        gr()
+      }
   }
+if(topdf) dev.off()
