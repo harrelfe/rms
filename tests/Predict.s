8ef43681a8303a9201d00216da7a8b24a2242799
Frank Harrell <harrelfe@HPTouchSmart>
2011-12-17 23:26:47 -0600

    Improved example of suppressing certain predictions

--- tests/Predict.s
+++ tests/Predict.s
@@ -9,4 +9,5 @@ p <- Predict(f, x1, x2, np=10)
 p[p$x2=='b' & p$x1 > .5,c('yhat','lower','upper')] <- NA
 # or p[with(p, x2=='b' & x1 > .5), 3:5] <- NA
 plot(p)
-
+p <- Predict(f, x1, x2, np=10)
+plot(p, subset=x2=='a' | (x2=='b' & x1 < .5))
