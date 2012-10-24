ec94eb29c494b843d20e63abb40c7eda34d6bd3f
Frank Harrell <harrelfe@harrelfe>
2012-10-24 17:38:48 -0500

    added type argument which specifies the default point type

--- R/plot.Predict.s
+++ R/plot.Predict.s
@@ -4,7 +4,7 @@ plot.Predict <-
            data=NULL, subdata, col.fill=gray(seq(.95, .75, length=5)),
            adj.subtitle, cex.adj, cex.axis, perim=NULL,
            digits=4, nlevels=3, nlines=FALSE, addpanel,
-           scat1d.opts=list(frac=0.025, lwd=0.3), ...)
+           scat1d.opts=list(frac=0.025, lwd=0.3), type=NULL, ...)
 {
   require(lattice)
   if(varypred)
@@ -157,8 +157,8 @@ plot.Predict <-
       scales <- list(x=list(relation='free', limits=limits,
                        at=at, labels=labels))
       if(!missing(cex.axis)) scales$x$cex <- cex.axis
-      r <- list(formula=formula, groups=gr, subset=subset, type='l',
-                method=if(conf.int) 'filled bands' else 'bars',
+      r <- list(formula=formula, groups=gr, subset=subset, type=if(length(type))type else 'l',
+                method=if(conf.int & (!length(type) || type != 'p')) 'filled bands' else 'bars',
                 col.fill=col.fill,
                 xlab='', ylab=ylab, ylim=ylim,
                 panel=pan, scales=scales)
@@ -259,7 +259,6 @@ plot.Predict <-
       xv <- x[[xvar]]
       xscale <- NULL
       xdiscrete <- (is.factor(xv) || is.character(xv)) && nlines
-      prn(attributes(xv)); prn(is.character(xv)); prn(nlines);prn(xdiscrete)
       if(xdiscrete)
         {
           xv <- as.factor(xv)
@@ -318,8 +317,8 @@ plot.Predict <-
         }
 
       r <- list(formula=formula, data=x, subset=subset,
-                type=if(xdiscrete) 'b' else 'l',
-                method=if(conf.int) 'filled bands' else 'bars',
+                type=if(length(type)) type else if(xdiscrete) 'b' else 'l',
+                method=if(conf.int & (!length(type) || type!='p')) 'filled bands' else 'bars',
                 col.fill=col.fill,
                 xlab=xlab, ylab=ylab, ylim=ylim, panel=pan)
       if(length(xscale)) r$scales <- xscale
