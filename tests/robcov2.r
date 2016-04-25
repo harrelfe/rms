47bfe5ad0f353e3707970bae0b09a25a6b3217f5
Frank Harrell <f.harrell@vanderbilt.edu>
2016-04-25 13:44:45 -0500

    tests/robcov2.r: clarified n.subjects

--- tests/robcov2.r
+++ tests/robcov2.r
@@ -1,12 +1,12 @@
 # From Yong Hao Pua <puayonghao@gmail.com>
 require(rms)
-n.subjects <- 51    # original number is 30 on the rms documentation
+n.subjects <- 100    # original number is 30 on the rms documentation
 ages <- rnorm(n.subjects, 50, 15)
 sexes <- factor(sample(c('female','male'), n.subjects, TRUE))
 logit <- (ages - 50) / 5
 prob <- plogis(logit) # true prob not related to sex
 id <- sample(1:n.subjects, 300, TRUE) # subjects sampled multiple times
-length(unique(id))
+length(unique(id))   # note that don't always get n.subjects sampled
 length(table(id))
 table(table(id)) # frequencies of number of obs/subject
 age <- ages[id]
@@ -15,5 +15,5 @@ sex <- sexes[id]
 y <- ifelse(runif(300) <= prob[id], 1, 0)
 f <- lrm(y ~ lsp(age, 50) * sex, x=TRUE, y=TRUE )
 g <- robcov(f, id)
-g$clusterInfo  # returns 51
+g$clusterInfo
 
