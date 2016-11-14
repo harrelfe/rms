3a9a32d5b54362f96a4449404dbce6908ec150b9
Frank Harrell <f.harrell@vanderbilt.edu>
2016-11-14 14:14:49 -0600

    inst/tests/ggplot2.r: fixed non-ggplot method test

--- inst/tests/ggplot2.r
+++ inst/tests/ggplot2.r
@@ -234,15 +234,15 @@ colorscale <- function(...)
                      values=c("#000000", "#E69F00", "#56B4E9",
                               "#009E73","#F0E442", "#0072B2", "#D55E00", "#CC79A7"))
  
-ggplot(p, aes(x=cholesterol, y=yhat, color=eyecolor, linetype=sex)) +
+ggplot(as.data.frame(p), aes(x=cholesterol, y=yhat, color=eyecolor, linetype=sex)) +
   labs(x=expression(cholesterol), y="log odds",
        title="Adjusted to:age=50.2 ")  +
   geom_line(data=p, mapping=aes(color=eyecolor, linetype=sex)) +
   colorscale(name=expression(eyecolor)) +
   scale_linetype_discrete(name=expression(sex)) +
   theme(legend.position='right') +
-  geom_ribbon(data=p, aes(ymin=lower, ymax=upper), alpha=0.2,
-              linetype=0, fill=I('black'), show.legend=FALSE) +
+#  geom_ribbon(data=p, aes(ymin=lower, ymax=upper), alpha=0.2,
+#              linetype=0, fill=I('black'), show.legend=FALSE) +
   coord_cartesian(ylim=c(-4, 6)) +
   theme(plot.title=element_text(size=8, hjust=1))
 
