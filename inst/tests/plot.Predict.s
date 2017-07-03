528ee891e8d93d1ef783979e3700f0fb5d08fe78
harrelfe <f.harrell@vanderbilt.edu>
2017-07-03 15:28:13 -0500

    inst/tests/plot.Predict.s: new rbind example that failed from Ferenci Tamas

--- inst/tests/plot.Predict.s
+++ inst/tests/plot.Predict.s
@@ -16,3 +16,27 @@ p <- Predict(f, x1=seq(0,1,by=.1), group)
 plot(p, ~ x1, groups='group', col=3:1)
 
 
+## From Ferenci Tamas <ferenci.tamas@nik.uni-obuda.hu>
+set.seed( 1 )
+d <- data.frame(x1 = rnorm( 1000 ),
+                x2 = sample( 1:2, 1000, replace = TRUE ),
+                x3 = sample( 1:2, 1000, replace = TRUE ) )
+d <- transform( d, y = x3*( x1 + x2 )+rnorm( 1000 ) )
+
+dd <- datadist( d )
+options( datadist = "dd" )
+
+fit <- ols( y ~ x3*( x1 + x2 ), data = d )
+
+p1 <- Predict( fit, x1, x3 )
+p2 <- Predict( fit, x2, x3 )
+p  <- rbind(x1=p1, b2=p2)
+plot(p, groups='x3', varypred=TRUE)
+
+#Now, if you run plot( p1 ) or plot( p2 ), everything is fine. However, in 
+#the last call above the panel for the continuous predictor, x1 is
+#fine, the same as plot( p1 ), but for the categorical predictor, it is
+#something completely different (and wrong, quite fundamentally: the
+#two groups do not even appear). 
+
+
