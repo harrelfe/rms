280ffe641ae7cc71425e890634cc62e3e7bd1726
harrelfe <f.harrell@vanderbilt.edu>
2018-07-11 13:28:05 -0500

    inst/tests/cph4.r: commented out failing line per Terry Therneau

--- inst/tests/cph4.r
+++ inst/tests/cph4.r
@@ -8,7 +8,7 @@ x3 <- runif(n)
 x4 <- sample(0:4, n, TRUE)
 x5 <- ordered(sample(1:3, n, TRUE), levels=1:3, labels=c('I','II','III'))
 S  <- Surv(runif(n))
-f <- cph(S ~ x1 + pol(x2, 2) + rcs(x3, 4) + scored(x4) + x5)
+# f <- cph(S ~ x1 + pol(x2, 2) + rcs(x3, 4) + scored(x4) + x5)   # FAILS
 options(contrasts=c('contr.treatment', 'contr.treatment'))
 f <- cph(S ~ x1 + pol(x2, 2) + rcs(x3, 4) + scored(x4) + x5)
 f
