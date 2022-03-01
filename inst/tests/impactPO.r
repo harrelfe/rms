560b3570714a3c6001c17c9dcb2311c5d9fdfdb2
harrelfe <fh@fharrell.com>
2022-02-28 22:06:17 -0600

    tests: added impactPO.r

---
+++ inst/tests/impactPO.r
@@ -0,0 +1,32 @@
+require(rms)
+require(nnet)
+set.seed(1)
+age <- rnorm(500, 50, 10)
+sex <- sample(c('female', 'male'), 500, TRUE)
+y   <- sample(0:4, 500, TRUE)
+d   <- expand.grid(age=50, sex=c('female', 'male'))
+
+w <- impactPO(y ~ age + sex, newdata=d)
+w
+# Note that PO model is a better model than multinomial (lower AIC)
+# since multinomial model's improvement in fit is low in comparison
+# with number of additional parameters estimated
+
+# Reverse levels of y so stacked bars have higher y located higher
+revo <- function(z) {
+  z <- as.factor(z)
+  factor(z, levels=rev(levels(as.factor(z))))
+}
+
+ggplot(w$estimates, aes(x=method, y=Probability, fill=revo(y))) +
+  facet_wrap(~ sex) + geom_col() +
+  xlab('') + guides(fill=guide_legend(title=''))
+
+d <- expand.grid(sex=c('female', 'male'), age=c(40, 60))
+w <- impactPO(y ~ age + sex, nonppo = ~ sex, newdata=d)
+w
+ggplot(w$estimates, aes(x=method, y=Probability, fill=revo(y))) +
+  facet_grid(age ~ sex) + geom_col() +
+  xlab('') + guides(fill=guide_legend(title=''))
+
+
