257f79cb7520ad3afc147ae2207c5482d2715290
harrelfe <fh@fharrell.com>
2022-03-01 13:20:02 -0600

    impactPO:better argument name for nonpo

--- R/impactPO.r
+++ R/impactPO.r
@@ -3,7 +3,7 @@
 #' Checks the impact of the proportional odds assumption by comparing predicted cell probabilities from a PO model with those from a multinomial or partial proportional odds logistic model.  For a given model formula, fits the model with both `lrm` and either `nnet::multinom` or `VGAM::vglm` and obtains predicted cell probabilities for both on the `newdata` data frame.
 #'
 #' @param formula a model formula.  To work properly with `multinom` or `vglm` the terms should have completely specified knot locations if a spline function is being used.
-#' @param nonppo a formula with no left hand side variable, specifying the variable or variables for which PO is not assumed.  Specifying `nonppo` results in the alternative (relaxed) fit being a partial PO model fitted with `VGAM::vglm`.  If `nonppo` is omitted, the multimonial model is fitted and PO is not assumed for any variables.
+#' @param nonpo a formula with no left hand side variable, specifying the variable or variables for which PO is not assumed.  Specifying `nonpo` results in the alternative (relaxed) fit being a partial PO model fitted with `VGAM::vglm`.  If `nonpo` is omitted, the multimonial model is fitted and PO is not assumed for any variables.
 #' @param newdata a data frame or data table with one row per covariate setting for which predictions are to be made
 #' @param ... other parameters to pass to `lrm` and `multinom` such as `data=`.
 #' @return an `impactPO` object which is a list with elements `estimates` and `stats`.  `data` is a data frame containing the variables and values in `newdata` in a tall and thin format with additional variable `method` ("PO", "multinomial", or "PPO"), `y` (current level of the dependent variable), and `Probability` (predicted cell probability for covariate values and value of `y` in the current row).
@@ -48,11 +48,11 @@
 #'  xlab('') + guides(fill=guide_legend(title=''))
 #'
 #' Relax PO assumption only for sex
-#' w <- impactPO(y ~ age + sex, nonppo = ~ sex, newdata=d)
+#' w <- impactPO(y ~ age + sex, nonpo = ~ sex, newdata=d)
 #' }
 
-impactPO <- function(formula, nonppo, newdata, ...) {
-  model <- if(missing(nonppo)) 'multinomial' else 'PPO'
+impactPO <- function(formula, nonpo, newdata, ...) {
+  model <- if(missing(nonpo)) 'multinomial' else 'PPO'
   switch(model,
          multinomial = if(! requireNamespace('nnet', quietly=TRUE))
                          stop("This function requires the 'nnet' package"),
@@ -67,7 +67,7 @@ impactPO <- function(formula, nonppo, newdata, ...) {
     b <- predict(g, newdata, 'probs')
     aic <- AIC(g)
   } else {
-    ppo  <- formula(paste('FALSE ~', as.character(nonppo)[-1]))
+    ppo  <- formula(paste('FALSE ~', as.character(nonpo)[-1]))
     g <- VGAM::vglm(formula, VGAM::cumulative(parallel=ppo, reverse=TRUE), ...)
     b <- VGAM::predict(g, newdata, type='response')
     aic <- deviance(g) + 2 * length(coef(g))
