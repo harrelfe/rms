2a94c19e3c01d39f7606f3a3956f102f5b68ea75
harrelfe <fh@fharrell.com>
2022-03-01 19:07:01 -0600

    impactPO: consolidated 2 relaxed model methods

--- R/impactPO.r
+++ R/impactPO.r
@@ -1,12 +1,13 @@
 #' Impact of Proportional Odds Assumpton
 
-#' Checks the impact of the proportional odds assumption by comparing predicted cell probabilities from a PO model with those from a multinomial or partial proportional odds logistic model.  For a given model formula, fits the model with both `lrm` and either `nnet::multinom` or `VGAM::vglm` and obtains predicted cell probabilities for both on the `newdata` data frame.
+#' Checks the impact of the proportional odds assumption by comparing predicted cell probabilities from a PO model with those from a multinomial or partial proportional odds logistic model that relax assumptions.  For a given model formula, fits the model with both `lrm` and either `nnet::multinom` or `VGAM::vglm` or both, and obtains predicted cell probabilities for the PO and relaxed models on the `newdata` data frame.
 #'
 #' @param formula a model formula.  To work properly with `multinom` or `vglm` the terms should have completely specified knot locations if a spline function is being used.
-#' @param nonpo a formula with no left hand side variable, specifying the variable or variables for which PO is not assumed.  Specifying `nonpo` results in the alternative (relaxed) fit being a partial PO model fitted with `VGAM::vglm`.  If `nonpo` is omitted, the multimonial model is fitted and PO is not assumed for any variables.
+#' @param relax defaults to `"both"` if `nonpo` is given, resulting in fitting two relaxed models.  Set `relax` to `"multinomial"` or `"ppo"` to fit only one relaxed model.  The multinomial model does not assume PO for any predictor.
+#' @param nonpo a formula with no left hand side variable, specifying the variable or variables for which PO is not assumed.  Specifying `nonpo` results in a relaxed fit that is a partial PO model fitted with `VGAM::vglm`.  
 #' @param newdata a data frame or data table with one row per covariate setting for which predictions are to be made
 #' @param ... other parameters to pass to `lrm` and `multinom` such as `data=`.
-#' @return an `impactPO` object which is a list with elements `estimates` and `stats`.  `data` is a data frame containing the variables and values in `newdata` in a tall and thin format with additional variable `method` ("PO", "multinomial", or "PPO"), `y` (current level of the dependent variable), and `Probability` (predicted cell probability for covariate values and value of `y` in the current row).
+#' @return an `impactPO` object which is a list with elements `estimates` and `stats`.  `data` is a data frame containing the variables and values in `newdata` in a tall and thin format with additional variable `method` ("PO", "multinomial", "PPO"), `y` (current level of the dependent variable), and `Probability` (predicted cell probability for covariate values and value of `y` in the current row).
 #'
 #' @author Frank Harrell <fh@fharrell.com>
 #'
@@ -47,52 +48,65 @@
 #'   facet_grid(age ~ sex) + geom_col() +
 #'  xlab('') + guides(fill=guide_legend(title=''))
 #'
-#' Relax PO assumption only for sex
+#' Relax PO assumption only for sex and also show multinomial estimates
 #' w <- impactPO(y ~ age + sex, nonpo = ~ sex, newdata=d)
+#' # use relax='ppo' to omit the multinomial model 
 #' }
 
-impactPO <- function(formula, nonpo, newdata, ...) {
-  model <- if(missing(nonpo)) 'multinomial' else 'PPO'
-  switch(model,
-         multinomial = if(! requireNamespace('nnet', quietly=TRUE))
-                         stop("This function requires the 'nnet' package"),
-         PPO         = if(! requireNamespace('VGAM', quietly=TRUE))
-                         stop("This function requires the 'VGAM' package"))
+impactPO <- function(formula,
+                     relax=if(missing(nonpo)) 'multinomial' else 'both',
+                     nonpo, newdata, ...) {
+
+  if(relax != 'multinomial' && missing(nonpo))
+    stop('must specify nonpo when relax is not "multinomial"')
+  if(relax != 'ppo') if(! requireNamespace('nnet', quietly=TRUE))
+                       stop("This function requires the 'nnet' package")
+  if(relax != 'multinomial') if(! requireNamespace('VGAM', quietly=TRUE))
+                       stop("This function requires the 'VGAM' package")
   
   f <- lrm(formula, ...)
   a <- predict(f, newdata, type='fitted.ind')
+  nam <- names(f$freq)
+  if(nrow(newdata) == 1) a <- matrix(a, nrow=1)
+  colnames(a) <- nam
+  A <- cbind(method='PO', newdata, a)
+  stats <- data.frame(method   = 'PO',
+                      Deviance = deviance(f)[2],
+                      `d.f.`   = length(coef(f)),
+                      AIC      = AIC(f))
 
-  if(model == 'multinomial') {
+  if(relax != 'ppo') {
     g <- nnet::multinom(formula, ..., trace=FALSE)
     b <- predict(g, newdata, 'probs')
-    aic <- AIC(g)
-  } else {
+    if(nrow(newdata) == 1) b <- matrix(b, nrow=1)
+    colnames(b) <- nam
+    A <- rbind(A, cbind(method='multinomial', newdata, b))
+    stats <- rbind(stats,
+                   data.frame(method   = 'multinomial',
+                              Deviance = deviance(g),
+                              `d.f.`   = length(coef(g)),
+                              AIC      = deviance(g) + 2 * length(coef(g))))
+  }
+
+  if(relax != 'multinomial') {
     ppo  <- formula(paste('FALSE ~', as.character(nonpo)[-1]))
     g <- VGAM::vglm(formula, VGAM::cumulative(parallel=ppo, reverse=TRUE), ...)
     b <- VGAM::predict(g, newdata, type='response')
-    aic <- deviance(g) + 2 * length(coef(g))
+    if(nrow(newdata) == 1) b <- matrix(b, nrow=1)
+    colnames(b) <- nam
+    A <- rbind(A, cbind(method='PPO', newdata, b))
+    stats <- rbind(stats,
+                   data.frame(method   = 'PPO',
+                              Deviance = deviance(g),
+                              `d.f.`   = length(coef(g)),
+                              AIC      = deviance(g) + 2 * length(coef(g))))
     }
   
-  nam <- names(f$freq)
-  if(nrow(newdata) == 1) {
-    a <- matrix(a, nrow=1)
-    b <- matrix(b, nrow=1)
-    }
-  colnames(a) <- colnames(b) <- nam
-
-  A <- cbind(method='PO',  newdata, a)
-  B <- cbind(method=model, newdata, b)
-  w <- rbind(A, B)
-
-  z <- reshape(w, direction='long', varying=list(nam),
+  z <- reshape(A, direction='long', varying=list(nam),
                times=nam, v.names='Probability', timevar='y')
-  z$method <- factor(z$method, c('PO', model))
+  z$method <- factor(z$method,
+   c('PO', c('PPO', 'multinomial')[c(relax != 'multinomial', relax != 'PPO')]))
 
-  k <- coef(g)
-  r <- data.frame(method   = c('PO', model),
-                  Deviance = c(deviance(f)[2], deviance(g)),
-                  `d.f.`   = c(length(coef(f)), length(coef(g))),
-                  AIC      = c(AIC(f), aic))
-  rownames(r) <- NULL
-  list(estimates=z, stats=r)
+  rownames(stats) <- NULL
+  list(estimates=z, stats=stats)
 }
