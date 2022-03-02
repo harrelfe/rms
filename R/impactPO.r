6a91e1f487ca01635a9a9fd03ca53d6c3cd80cd6
harrelfe <fh@fharrell.com>
2022-03-02 07:48:21 -0600

    impactPO: added LR chisq and R2

--- R/impactPO.r
+++ R/impactPO.r
@@ -7,7 +7,7 @@
 #' @param nonpo a formula with no left hand side variable, specifying the variable or variables for which PO is not assumed.  Specifying `nonpo` results in a relaxed fit that is a partial PO model fitted with `VGAM::vglm`.  
 #' @param newdata a data frame or data table with one row per covariate setting for which predictions are to be made
 #' @param ... other parameters to pass to `lrm` and `multinom` such as `data=`.
-#' @return an `impactPO` object which is a list with elements `estimates` and `stats`.  `data` is a data frame containing the variables and values in `newdata` in a tall and thin format with additional variable `method` ("PO", "multinomial", "PPO"), `y` (current level of the dependent variable), and `Probability` (predicted cell probability for covariate values and value of `y` in the current row).
+#' @return an `impactPO` object which is a list with elements `estimates` and `stats`.  `data` is a data frame containing the variables and values in `newdata` in a tall and thin format with additional variable `method` ("PO", "multinomial", "PPO"), `y` (current level of the dependent variable), and `Probability` (predicted cell probability for covariate values and value of `y` in the current row).  `stats` is a data frame containing `Deviance` the model deviance, `d.f.` the total number of parameters counting intercepts, `AIC`, `p` the number of regression coefficients, `LR chi^2` the likelihood ratio chi-square statistic for testing the predictors, `LR - p` a chance-corrected LR chi-square, `R2` the Nagelkerke pseudo R-squared, and `R2 adj` a chance-corrected pseudo R-squared using `LR - p`.
 #'
 #' @author Frank Harrell <fh@fharrell.com>
 #'
@@ -25,11 +25,12 @@
 #' sex <- sample(c('female', 'male'), 500, TRUE)
 #' y   <- sample(0:4, 500, TRUE)
 #' d   <- expand.grid(age=50, sex=c('female', 'male'))
-#' w   <- impactPO(y ~ age + sex, newdata=d)
+#' w   <- impactPO(y ~ age + sex, nonpo = ~ sex, newdata=d)
 #' w
 #' # Note that PO model is a better model than multinomial (lower AIC)
 #' # since multinomial model's improvement in fit is low in comparison
-#' # with number of additional parameters estimated
+#' # with number of additional parameters estimated.  Same for PO model
+#' # in comparison with partial PO model.
 #'
 #' # Reverse levels of y so stacked bars have higher y located higher
 #' revo <- function(z) {
@@ -41,16 +42,14 @@
 #'   facet_wrap(~ sex) + geom_col() +
 #'   xlab('') + guides(fill=guide_legend(title=''))
 #'
+#' # Now vary 2 predictors
+#' 
 #' d <- expand.grid(sex=c('female', 'male'), age=c(40, 60))
-#' w <- impactPO(y ~ age + sex, newdata=d)
+#' w <- impactPO(y ~ age + sex, nonpo = ~ sex, newdata=d)
 #' w
 #' ggplot(w$estimates, aes(x=method, y=Probability, fill=revo(y))) +
 #'   facet_grid(age ~ sex) + geom_col() +
 #'  xlab('') + guides(fill=guide_legend(title=''))
-#'
-#' Relax PO assumption only for sex and also show multinomial estimates
-#' w <- impactPO(y ~ age + sex, nonpo = ~ sex, newdata=d)
-#' # use relax='ppo' to omit the multinomial model 
 #' }
 
 impactPO <- function(formula,
@@ -70,23 +69,31 @@ impactPO <- function(formula,
   if(nrow(newdata) == 1) a <- matrix(a, nrow=1)
   colnames(a) <- nam
   A <- cbind(method='PO', newdata, a)
-  stats <- data.frame(method   = 'PO',
-                      Deviance = deviance(f)[2],
-                      `d.f.`   = length(coef(f)),
-                      AIC      = AIC(f))
-
-  if(relax != 'ppo') {
-    g <- nnet::multinom(formula, ..., trace=FALSE)
-    b <- predict(g, newdata, 'probs')
-    if(nrow(newdata) == 1) b <- matrix(b, nrow=1)
-    colnames(b) <- nam
-    A <- rbind(A, cbind(method='multinomial', newdata, b))
-    stats <- rbind(stats,
-                   data.frame(method   = 'multinomial',
-                              Deviance = deviance(g),
-                              `d.f.`   = length(coef(g)),
-                              AIC      = deviance(g) + 2 * length(coef(g))))
-  }
+  dev  <- deviance(f)
+  dev0 <- dev[1]
+  n    <- f$stats['Obs']
+  kint <- length(nam) - 1
+  st <- function(method, fit) {
+    df  <- length(coef(fit))
+    p   <- df - kint
+    dev <- deviance(fit)
+    dev <- dev[length(dev)]
+    aic <- dev + 2 * df
+    LR  <- dev0 - dev
+    r2max <- 1 - exp(- dev0 / n)
+    r2    <- (1 - exp(- LR / n)) / r2max
+    r2adj <- (1 - exp(- max(0, LR - p) / n)) / r2max
+    data.frame(method     = method,
+               Deviance   = dev,
+               `d.f.`     = df,
+               AIC        = aic,
+               p          = p,
+               `LR chi^2` = LR,
+               `LR - p`   = LR - p,
+               R2         = r2,
+               `R2 adj`   = r2adj, check.names=FALSE)
+    }
+  stats <- st('PO', f)
 
   if(relax != 'multinomial') {
     ppo  <- formula(paste('FALSE ~', as.character(nonpo)[-1]))
@@ -95,13 +102,18 @@ impactPO <- function(formula,
     if(nrow(newdata) == 1) b <- matrix(b, nrow=1)
     colnames(b) <- nam
     A <- rbind(A, cbind(method='PPO', newdata, b))
-    stats <- rbind(stats,
-                   data.frame(method   = 'PPO',
-                              Deviance = deviance(g),
-                              `d.f.`   = length(coef(g)),
-                              AIC      = deviance(g) + 2 * length(coef(g))))
+    stats <- rbind(stats, st('PPO', g))
     }
-  
+
+  if(relax != 'ppo') {
+    g <- nnet::multinom(formula, ..., trace=FALSE)
+    b <- predict(g, newdata, 'probs')
+    if(nrow(newdata) == 1) b <- matrix(b, nrow=1)
+    colnames(b) <- nam
+    A <- rbind(A, cbind(method='multinomial', newdata, b))
+    stats <- rbind(stats, st('multinomial', g))
+  }
+
   z <- reshape(A, direction='long', varying=list(nam),
                times=nam, v.names='Probability', timevar='y')
   z$method <- factor(z$method,
--- inst/tests/impactPO.r
+++ inst/tests/impactPO.r
@@ -1,16 +1,16 @@
 require(rms)
-require(nnet)
 set.seed(1)
 age <- rnorm(500, 50, 10)
 sex <- sample(c('female', 'male'), 500, TRUE)
 y   <- sample(0:4, 500, TRUE)
 d   <- expand.grid(age=50, sex=c('female', 'male'))
 
-w <- impactPO(y ~ age + sex, newdata=d)
+w <- impactPO(y ~ age + sex, nonpo = ~ sex, newdata=d)
 w
 # Note that PO model is a better model than multinomial (lower AIC)
 # since multinomial model's improvement in fit is low in comparison
-# with number of additional parameters estimated
+# with number of additional parameters estimated.  The same is true
+# in comparison to the partial PO model.
 
 # Reverse levels of y so stacked bars have higher y located higher
 revo <- function(z) {
@@ -23,10 +23,8 @@ ggplot(w$estimates, aes(x=method, y=Probability, fill=revo(y))) +
   xlab('') + guides(fill=guide_legend(title=''))
 
 d <- expand.grid(sex=c('female', 'male'), age=c(40, 60))
-w <- impactPO(y ~ age + sex, nonppo = ~ sex, newdata=d)
+w <- impactPO(y ~ age + sex, nonpo = ~ sex, newdata=d)
 w
 ggplot(w$estimates, aes(x=method, y=Probability, fill=revo(y))) +
   facet_grid(age ~ sex) + geom_col() +
   xlab('') + guides(fill=guide_legend(title=''))
-
-
