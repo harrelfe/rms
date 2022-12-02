02839aae4037e090753e54f23e0afa661e7d6231
harrelfe <fh@fharrell.com>
2022-12-02 10:12:33 -0600

    orm.Rd: next example of plotting exceedance probabilities tset/rms-lm.r: new test of rcs outside of rms

---
+++ inst/tests/rms-lm.r
@@ -0,0 +1,14 @@
+# From Shawn Garbett 2022-11-21
+require(rms)
+x <- (1:200 + rnorm(200)) / 50
+y <- 5*sin(x) + x^2 + rnorm(100)
+plot(x, y, main="test data")
+lines(x, 5*sin(x)+x^2)
+model <- lm(y ~ rcs(x,3))
+summary(model)
+stripped <- strip(model, "predict")
+x <- seq(0, 4, by=0.2)
+plot(x, predict(stripped, data.frame(x=x)), main="Predicted vs. Truth",
+     ylab="Predicted Y")
+lines(x, 5*sin(x)+x^2, col='red')
+
