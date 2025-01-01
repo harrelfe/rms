e4b735498e240b9675cfb430a88b956f8766b63c
harrelfe <fh@fharrell.com>
2025-01-01 15:26:38 +0100

    infoMxop: added extremely fast way to compute variance matrix for only the betas

--- R/infoMxop.r
+++ R/infoMxop.r
@@ -2,6 +2,8 @@
 #'
 #' Processes three types of information matrices: ones produced by the `SparseM` package for the `orm` function in `rms` version 6.9-0 and earlier, by the `Matrix` package for version 7.0-0 of `rms`, or plain matrices.  For `Matrix`, the input information matrix is a list with three elements: `a` containing in two columns the diagonal and superdiagonal for intercepts, `b`, a square matrix for the covariates, and `ab` for intercepts x covariates.  If nothing else is specified, the assembled information matrix is returned for `Matrix`, or the original `info` otherwise.  If `p=TRUE`, the number of parameters in the model (number of rows and columns in the whole information matrix) is returned.  If `i` is given, the `i` elements of the inverse of `info` are returned, using efficient calculation to avoid inverting the whole matrix.  Otherwise if `invert=TRUE` or `B` is given without `i`, the efficiently (if `Matrix` or `SparseM`) inverted matrix is returned, or the matrix multiplication of the inverse and `B`.  If both `i` and `B` are given, what is returned is the `i` portion of the inverse of the information matrix, matrix multiplied by `B`.  This is done inside `solve()`.
 #' 
+#' When only variance-covariance matrix elements corresponding to the non-intercepts are desired, specify
+#' `i='x'` or `i=(k + 1) : nv` where `nv` is the number of intercepts and slopes combined.  `infoMxop` computes the needed covariance matrix very quickly in this case.
 #' When inverting `info`, if `info` has a `'scale'` attribute with elements `mean` and `sd`, the scaling is reversed after inverting `info`.
 #'
 #' @param info an information matrix object
@@ -22,11 +24,14 @@
 #' f <- orm(y ~ x)
 #' infoMxop(f$info.matrix)   # assembles 3 pieces
 #' infoMxop(v, i=c(2,4))     # returns a submatrix of v inverse
+#' infoMxop(f$info.matrix, i='x')  # sub-covariance matrix for just the betas
 #' }
 infoMxop <- function(info, i, invert=! missing(i) || ! missing(B),
-                     B=NULL, np=FALSE, tol=1e-14, abort=TRUE) {
+                     B, np=FALSE, tol=1e-14, abort=TRUE) {
   if(! missing(i) && ! invert)
     stop('i is irrelevant if invert=FALSE')
+  Bp <- ! missing(B) 
+  if(Bp) B <- Matrix::Matrix(B)
   
   xname <- iname <- name <- sc <- NULL
   if(is.matrix(info)) name <- colnames(info)
@@ -79,13 +84,11 @@ infoMxop <- function(info, i, invert=! missing(i) || ! missing(B),
                   SparseM = SparseM::as.matrix,
                   Matrix  = Matrix ::as.matrix)
   if(missing(i)) {
-    v <- if(length(B)) tryit(asm(solv(info, B, tol=tol))) else tryit(asm(solv(info, tol=tol)))
+    v <- if(Bp) tryit(asm(solv(info, B, tol=tol))) else tryit(asm(solv(info, tol=tol)))
     if(length(sc)) v <- t(trans) %*% v %*% trans
   }
   else {
     # User has specied i, a vector of indexes of rows/columns of inverse to keep
-    # Construct w = a p x r matrix where r = no. desired inverse elements
-    # jth column of w has a 1 in i(j) row
     if(is.character(i) && length(i) == 1) {
       if(! t3) k <- attr(info, 'intercepts')
       if(! length(k)) 
@@ -96,20 +99,33 @@ infoMxop <- function(info, i, invert=! missing(i) || ! missing(B),
                   i = 1 : k,
                   x = (k + 1) : nv)
     }
+    if((length(i) == nv - k) && all(sort(i) == (k + 1) : nv)) {
+      # It's very quick to only get the beta components of the inverse
+      # It's slower to do likewise for just the intercept components; best to
+      # just use the i=1:k for that
+      M <- b - Matrix::t(ab) %*% solv(a, ab, tol=tol)
+      v <- if(Bp) solv(M, B, tol=tol) else solv(M, tol=tol)
+      if(length(sc)) {
+        w <- trans[i, i, drop=FALSE]
+        v <- t(w) %*% v %*% w
+      }
+    if(! Bp) dimnames(v) <- list(name[i], name[i])
+    return(v)
+    }
+
+    # Construct w = a p x r matrix where r = no. desired inverse elements
+    # jth column of w has a 1 in i(j) row
     l <- length(i)
     w <- matrix(0., nv, l)
     w[cbind(i, 1 : l)] <- 1
     if(type == 'Matrix') w <- Matrix::Matrix(w)
-    if(! missing(B)) {
-      B <- Matrix::Matrix(B)
-      w <- w %*% B
-    }
+    if(Bp) w <- w %*% B
     v <- tryit(asm(solv(info, w, tol=tol)[i, , drop=FALSE]))
     if(length(sc)) {
       w <- trans[i, i, drop=FALSE]
       v <- t(w) %*% v %*% w
     }
-    if(missing(B)) dimnames(v) <- list(name[i], name[i])
+    if(! Bp) dimnames(v) <- list(name[i], name[i])
   }
   v
 }
