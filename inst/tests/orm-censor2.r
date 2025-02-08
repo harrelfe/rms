599577d222cb1a6c00499032c2d12ea3e3ef3276
harrelfe <fh@fharrell.com>
2025-02-08 14:06:42 -0600

    orm-censor2, 3: tests for interval censoring with orm

---
+++ inst/tests/orm-censor2.r
@@ -0,0 +1,48 @@
+# Check that different configurations of censoring are handled correctly by ormll.f90
+# used by orm.fit
+# Turn on debugging so that ormll will print subscripts of first terms (ia) and of
+# second per-observation terms (ia2) plus the sign used for the first term
+# sgn = -1  for Pr(Y = 0) or for left censoring
+
+require(rms)
+options(orm.fit.debug=TRUE)
+
+unp <- function(x) {
+  i <- grep(paste0('^', x, '$'), ormout)[1] + 1
+  x <- ormout[i]
+  x <- gsub('  ', ' ', x)
+  x <- sub('[1] ', '', x, fixed=TRUE)
+  as.numeric(strsplit(x, split=' ')[[1]])
+}
+
+w <- function(a, b) {
+  Y <- Ocens(a, b, verbose=TRUE)
+  f <- attr(Y, 'npsurv')
+  y <- Y
+  attributes(y) <- attributes(Y)['dim']
+  u <- orm.fit(y = Y, onlydata=TRUE)
+  with(u, cat('\nk=', u$k, ' Ncens=', u$Ncens, '\n'))
+  with(f, prn(cbind(time, surv), 'npsurv'))
+  ormout <<- capture.output(fit <<- orm.fit(y = Y))
+  p <- c(1, plogis(coef(fit)))
+  prn(cbind(time=fit$yunique, upper=fit$yupper, surv=p), 'orm.fit')
+  ia  <- unp('ia')
+  ia2 <- unp('ia2')
+  sgn <- unp('sgn')
+  d <- data.frame(a, b, y, u$Y)
+  cat('\n')
+  print(d)
+  cat('\n')
+  print(data.frame(ia, ia2, sgn))
+  # cat(z[c(ia, ia + 1, ia2, ia2 + 1, sgn, sgn + 1)], sep='\n')
+}
+
+# Censoring beyond uncensored values
+w(c(-Inf, 1, 2, 3, 4), c(0, 1, 2, 3, Inf))
+
+# Censoring at outer uncensored values
+w(c(-Inf, 1, 2, 3, 3), c(1, 1, 2, 3, Inf))
+
+# Add interior L and R censored values untied with uncensored values
+w(c(-Inf, 1, -Inf,  2, 2.5, 3, 3), c(1, 1, 1.5, 2, Inf, 3, Inf))
+
---
+++ inst/tests/orm-censor3.r
@@ -0,0 +1,78 @@
+# Example agreement of orm's estimated survival curves and icenReg's
+# with a series of random datasets with left, right, and interval censoring
+
+require(rms)
+require(icenReg)
+Y7 <- NULL
+for(irep in 1 : 25) {
+cat('\n----------------------------------------------------\nRepetition', irep, '\n')
+set.seed(20 + irep)
+N <- 80
+y  <- sample(10 : 50, N, TRUE)
+maxy <- max(y)
+y2 <- y
+# Left censoring
+y[1:5] <- -Inf
+# Right censoring
+y2[6:10] <- Inf
+# Interval censoring
+y2[11:15] <- pmin(maxy, y[11:15] + sample(2 : 10, 5, TRUE))
+# if(irep == 1) saveRDS(list(y, y2), 'orm-censor3-problem-data.rds')
+if(irep == 7) {
+  i <- order(y)
+  Y7 <- cbind(y, y2)[i, ]
+  print(Y7)
+}
+
+d <- data.frame(y, y2, grp=factor(rep(1, N)))
+f <- ic_np(cbind(y, y2) ~ grp, data=d)
+s <- f$scurves[[1]]
+# print(data.frame(s$Tbull_ints, s$S_curves$baseline)[1:5,])
+plot(f, plot_legend=FALSE, main=paste('Example', irep))
+
+s <- Ocens(y, y2, nponly=TRUE)
+# print(data.frame(time=c(s$time[1], s$time), surv=c(s$surv, NA))[1:5, ])
+lines(c(s$time[1], s$time), c(s$surv, NA), type='s', col='green')
+Y <- Ocens(y, y2, verbose=FALSE)
+s <- attr(Y, 'npsurv')
+# print(data.frame(time=s$time, surv=s$surv)[1:5,])
+
+lines(c(s$time[1], s$time), c(s$surv, NA), type='s', col='yellow')
+
+options(orm.fit.debug=FALSE)
+f <- orm.fit(y=Y, trace=1)
+lines(c(f$yunique[1], f$yunique), c(1, plogis(coef(f)), NA), type='s', col='blue')
+}
+
+# See what happens when interval consolidation is not done
+y  <- Y7[, 1]
+y2 <- Y7[, 2]
+Y <- Ocens(y, y2)
+Y <- Ocens(y, y2, cons='none')
+np <- attr(Y, 'npsurv')
+with(np, cbind(time, surv, c(NA, diff(surv) == 0)))
+Y7[y %in% 26:27,]
+# Compute initial values forcing them to be in order
+init <- qlogis(np$surv[-1])
+init[12] <- 0.72
+f <- orm.fit(y=Y, initial=init, trace=2, maxit=1)
+im <- f$info.matrix
+a <- im$a
+with(a, plot(row, col))
+#Y[11:15,]
+require(Matrix)
+v <- infoMxop(im)
+diag(v)
+dg <- a$row == a$col
+plot(a$row[dg], a$a[dg])
+plot(a$row[! dg], a$a[! dg])
+d <- data.frame(row=a$row, col=a$col, a=a$a)
+subset(d, abs(a) < 0.1)
+subset(d, row %in% 10:13 | col %in% 10:13)
+vi <- solve(v)
+diag(vi)
+vi <- as.matrix(vi)
+plot(1 : nrow(vi), diag(vi))
+co <- cov2cor(vi)
+plotCorrM(co)
+co[29:32,29:32]   # r=0.994 for (30, 31)
