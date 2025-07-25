ee62c723fb0b77621ec697ed87aad5f232fbd7e7
harrelfe <fh@fharrell.com>
2025-07-25 16:54:44 -0500

    Olinks: added ... argument to pass to orm.fit

--- R/Olinks.r
+++ R/Olinks.r
@@ -3,6 +3,7 @@
 #' @param object an object created by `orm` with `x=TRUE, y=TRUE`
 #' @param links a vector of links to consider other than the one used to get `object`
 #' @param dec number of digits to the right of the decimal place to round statistics to
+#' @param ... passed to [orm.fit()]
 #'
 #' @returns data frame.  The `R2` column is from the last adjusted \eqn{R^2} computed by `orm`,
 #' which adjustes for the effective sample size and the number of betas.
@@ -15,7 +16,7 @@
 #' f <- orm(y ~ x1 + x2, family='loglog', x=TRUE, y=TRUE)
 #' Olinks(f)
 #' }
-Olinks <- function(object, links=c('logistic', 'probit', 'loglog', 'cloglog'), dec=3) {
+Olinks <- function(object, links=c('logistic', 'probit', 'loglog', 'cloglog'), dec=3, ...) {
   if(! inherits(object, 'orm')) stop('object must an orm object')
   if(! all(c('x', 'y') %in% names(object))) stop('must run orm with x=TRUE, y=TRUE')
   fam    <- object$family
@@ -24,7 +25,7 @@ Olinks <- function(object, links=c('logistic', 'probit', 'loglog', 'cloglog'), d
   fitter <- quickRefit(object, storevals=TRUE, compstats=TRUE, what='fitter')
   R      <- NULL
   for(fm in links) {
-    f    <- if(fm ==fam) object else fitter(family=fm)
+    f    <- if(fm ==fam) object else fitter(family=fm, ...)
     dev  <- deviance(f)
     st   <- f$stats
     r2   <- st[grep('^R2', names(st))]
