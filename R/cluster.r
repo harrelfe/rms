412af3ffc25e0a4b5c3129eff1b3db2a6f5c280d
harrelfe <fh@fharrell.com>
2026-09-05 14:50:24 -0500

    "cluster: new identify function called from within orm formulas to specify random effects"

---
+++ R/cluster.r
@@ -0,0 +1,10 @@
+##' Cluster Function for Random Effects
+##'
+##' Used by `blrm` to signal a categorical variable to generate random effects.
+##' @title cluster
+##' @param x a vector representing a categorical vector
+##' @return x unchanged
+##' @author Frank Harrell
+##' @md
+##' @export
+cluster <- function(x) x
